a72e6b43b3836a371b679458b570e869f2cde85a
HashBug <rkprince92@gmail.com>
2021-01-17 18:43:29 +0530

    added header component, nav links and ignored eslint cache

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import { Switch, Route } from "react-router-dom";
 import Shop from "./pages/shop/shop";
+import Header from "./components/header/Header";
 
 const Hats = () => <h1>Hats</h1>;
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={Shop} />
---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+const Header = () => (
+  <div className="header">
+    <Link to="/" className="logo-container">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        Shop
+      </Link>
+      <Link className="option" to="/contact">
+        Contact
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
